06501e780b01febd5dbe1251a31aba80c482cef8
Kai-Moriuchi <b1017184@fun.ac.jp>
2019-11-15 00:46:16 +0900

    [add] データの受け渡しの実装
    Cloud Firestoreへのデータ追加を実装。また、入力チェック機能も追加した。
    また、ListTileでstaffとなっており、エラーが表示されたのでをstuffに統一した。
    Cloud Firestoreも同じく変更した。

--- firebase_test/lib/main.dart
+++ firebase_test/lib/main.dart
@@ -42,7 +42,7 @@ class _MyInputFormState extends State<InputForm>{
     );
   }
 
-  //貸した、借りた情報をセットする
+  //貸した、借りたボタン情報をセットする
   void _setLendOrRent(String value){
     setState(() {
       _data.info = value;
@@ -51,6 +51,9 @@ class _MyInputFormState extends State<InputForm>{
 
   @override
   Widget build(BuildContext context) {
+    DocumentReference _mainReference;
+    _mainReference = Firestore.instance.collection('borrow_info').document();//インスタンスを生成
+
     return Scaffold(
       appBar: AppBar(
         title: Text("貸し借り入力"),
@@ -60,6 +63,19 @@ class _MyInputFormState extends State<InputForm>{
               icon: Icon(Icons.save),
               onPressed: () {
                 print("保存完了");
+                if (_formKey.currentState.validate()) {//エラー文表示のためのvalidate()を呼び出す
+                  _formKey.currentState.save();//名前、状態のonSave()を呼び出す
+                  //Cloud Firestore へのデータ登録
+                  _mainReference.setData(
+                      {
+                        'info': _data.info,
+                        'user': _data.user,
+                        'stuff': _data.stuff,
+                        'date': _data.date,
+                      }
+                  );
+                  Navigator.pop(context);//元の画面に戻る
+                }
               }
           ),
           IconButton(
@@ -104,7 +120,16 @@ class _MyInputFormState extends State<InputForm>{
                     hintText: '相手の名前',
                     labelText: 'Name',
                   ),
-                  ),
+                  onSaved: (String value) {//値の代入
+                    _data.user = value;
+                  },
+                  validator: (value) {//空欄時にエラー文を表示
+                    if (value.isEmpty) {
+                      return '名前は入力必須です';
+                    }
+                  },
+                  initialValue: _data.user,//初期値の設定
+                ),
 
                 TextFormField(
                   decoration: const InputDecoration(
@@ -112,6 +137,15 @@ class _MyInputFormState extends State<InputForm>{
                     hintText: '借りたもの、貸したもの',
                     labelText: 'Ioan',
                   ),
+                  onSaved: (String value) {//値の代入
+                    _data.stuff = value;
+                  },
+                  validator: (value) {//空欄時にエラー文を表示
+                    if (value.isEmpty) {
+                      return '借りたもの、貸したものは入力必須です';
+                    }
+                  },
+                  initialValue: _data.stuff,//初期値の設定
                 ),
 
                 Padding(
@@ -192,7 +226,7 @@ class _MyList extends State<List> {
             title: Text("【 " +
                 (document['info'] == "lend" ? "貸" : "借") +
                 " 】" +
-                document['staff']),
+                document['stuff']),
             subtitle: Text('期限 ： ' + document["date"].toString()/*.substring(0, 10)*/ +
                 //DateTime.fromMillisecondsSinceEpoch(document['date']).toString() + TimeStampからDateTimeへの変換
                 " \n相手 ： " + document['user']),
